eda5fd82281f7f4771ced3beb0012d1645b7c895
Rahul Dhuri <rdhuri@equalexperts.com>
2019-06-17 13:33:27 +0530

    NINGG-3585: Fix total mismatch issue in apple pay

--- PaymentSDK/UIHostObjCApp/sdk/ApplePaySDKDelegate.m
+++ PaymentSDK/UIHostObjCApp/sdk/ApplePaySDKDelegate.m
@@ -7,19 +7,22 @@
 //
 
 #import "ApplePaySDKDelegate.h"
-
+#import <PassKit/PassKit.h>
 @implementation ApplePaySDKDelegate
 
 - (void)applePayContactUpdatedWithDidSelect:(PKContact * _Nonnull)shippingContact handler:(void (^ _Nonnull)(PKPaymentRequestShippingContactUpdate * _Nonnull))completion {
-    
+    NSLog(@"Apple pay contact updated: \(shippingContact)");
+    completion([[PKPaymentRequestShippingContactUpdate alloc] initWithErrors: nil paymentSummaryItems: [NSArray new] shippingMethods: [NSArray new]]);
 }
 
 - (void)applePayPaymentMethodUpdatedWithDidSelect:(PaymentMethod * _Nonnull)paymentMethod handler:(void (^ _Nonnull)(PKPaymentRequestPaymentMethodUpdate * _Nonnull))completion {
-    
+    NSLog(@"Apple pay payment method updated: \(paymentMethod)");
+    completion([[PKPaymentRequestPaymentMethodUpdate alloc] initWithPaymentSummaryItems: [NSArray new]]);
 }
 
 - (void)applePayShippingMethodUpdatedWithDidSelect:(PKShippingMethod * _Nonnull)shippingMethod handler:(void (^ _Nonnull)(PKPaymentRequestShippingMethodUpdate * _Nonnull))completion {
-    
+    NSLog(@"Apple pay shipping method updated: \(shippingMethod)");
+    completion([[PKPaymentRequestShippingMethodUpdate alloc] initWithPaymentSummaryItems: [NSArray new]]);
 }
 
 @end
