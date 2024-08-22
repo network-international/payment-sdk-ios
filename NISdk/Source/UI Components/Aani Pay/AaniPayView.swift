6b25e709b2927e673691e7095eaff29f6db86ce0
Gautam <gautam.chibde@network.global>
2024-08-22 12:49:18 +0530

    NINGG-11411 - fix validation for mobile number, added error handling

--- NISdk/Source/UI Components/Aani Pay/AaniPayView.swift
+++ NISdk/Source/UI Components/Aani Pay/AaniPayView.swift
@@ -11,19 +11,13 @@ struct AaniPayView: View {
     @ObservedObject var viewModel: AaniViewModel
     
     var body: some View {
-        let direction: LayoutDirection = if (Locale.characterDirection(forLanguage: NISdk.sharedInstance.sdkLanguage) == .rightToLeft) {
-            .rightToLeft
-        } else {
-            .leftToRight
-        }
         switch viewModel.viewType {
         case .inputSelection:
             AaniInputScreen { type, text in
                 viewModel.onSubmit(idType: type, inputText: text)
-            }.environment(\.layoutDirection, direction)
+            }
         case .timer:
             AaniTimerScreen(amountFormatted: viewModel.getAmountFormatted(), timeString: viewModel.timeString)
-                .environment(\.layoutDirection, direction)
         }
     }
 }
